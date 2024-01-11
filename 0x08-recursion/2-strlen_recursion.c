14db6a8479d3b857a540d4d4709d54190fa915e7
MeggieR <mrotrigs@gmail.com>
2024-01-11 08:30:32 -0800

    Function to count strings

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - for reversing text
+ * @s: text to be reverses
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return 1 + _strlen_recursion(s +1);
+}
