fa21b565208ed8f7e358702203cb8fe9aec4f625
MeggieR <mrotrigs@gmail.com>
2024-01-22 11:36:08 -0800

    struct def

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+*struct dog - contains the dog's detail
+*@name: the dog name
+*@age: the dog's age
+*@owner: the dog's owner name
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
