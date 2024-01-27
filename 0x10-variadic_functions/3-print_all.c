c16ebc74c249fbdebcec4ca6bf81b25d9eaaa0ec
MeggieR <mrotrigs@gmail.com>
2024-01-26 18:56:57 -0800

    fin

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+*print_all - prints
+*@format: list arguments
+*/
+void print_all(const char * const format, ...)
+{
+	int index = 0
+		char *str, *sep = "";
+
