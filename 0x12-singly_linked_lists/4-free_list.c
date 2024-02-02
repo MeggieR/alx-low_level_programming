0f0b2081df70be2bd786a25dd93b651d4182561e
MeggieR <mrotrigs@gmail.com>
2024-02-02 06:21:59 -0800

    list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+*free_list - function for freeing list
+*@head: first node pointer
+**/
+void free_list(list_t *head)
+{
+	list_t temporary;
+
+	if (head == NULL)
+		return;
+	while (head->next != NULL)
+	{
+		temporary = head->next;
+		free(head->str);
+		free(head);
+		head = temporary;
+	}
+	free(head->str);
+	free(head);
+}
