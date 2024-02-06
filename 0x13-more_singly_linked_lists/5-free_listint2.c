3a9226a9f29c55f4a236f5d03766503ed79576a1
MeggieR <mrotrigs@gmail.com>
2024-02-06 04:35:07 -0800

    correction

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,11 +8,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *node_temp;
 
-	while (*head != NULL)
+	if (head == NULL)
+		return;
+	while (*head)
 	{
-		node_temp = *head;
-		*head = (*head)->next;
-		free(node_temp);
+		node_temp = (*head)->next;
+		free(*head);
+		*head = node_temp;
 	}
 	*head = NULL;
 }
